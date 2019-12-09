e3f5f17aead57da975a497c23b9187a40720950c
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2019-12-09 16:27:02 -0500

    updates

--- Implemenations/Queue.js
+++ Implemenations/Queue.js
@@ -4,11 +4,13 @@ class Queue {
     this.count = 0;
   }
 
+  // O(1)
   enqueue (item) {
     this.storage.push(item);
     this.count++;
   }
 
+  // O(N) for this implementation
   dequeue () {
     const removedItem = this.storage.shift();
     this.count--;
@@ -16,21 +18,27 @@ class Queue {
     return removedItem;
   }
 
-  front () {
+  // O(1)
+  peek () {
     return this.storage[0];
   }
 
+  // O(1)
   back () {
     return this.storage[this.count - 1];
   }
 
+  // O(1)
   isEmtpy () {
     return this.count === 0;
   }
 
+  // O(1)
   size () {
     return this.count;
   }
+
+  // isFull not implemented, we don't have fixed size arrays in JS
 }
 
 export default Queue;
