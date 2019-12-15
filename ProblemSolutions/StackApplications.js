0b3b39a0d2f8b5cb2ce59f7dee59105b96da5533
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2019-12-15 12:43:23 -0500

    Better isBalanced solution

--- ProblemSolutions/StackApplications.js
+++ ProblemSolutions/StackApplications.js
@@ -28,41 +28,50 @@ function stringReversal(str) {
 
 // console.log(stringReversal('abcd'));
 
-// This solution is shit
-function isBalanced(str) {
-  // https://stackoverflow.com/questions/4547609/how-do-you-get-a-string-to-a-character-array-in-javascript/34717402#34717402
-  const charArr = str.split('');
-  const stack = new Stack();
-  const leftBrackets = ['(', '<', '[', '{'];
-  const rightBrackets = [')', '>', ']', '}'];
+/**
+ * init Stack
+ * Build a map, helps with what 'type' of bracket it is and its orientation
+ * Process each bracket
+ * If we encounter an opening bracket, we simply push it onto the stack.
+ * This means we will process it later, let us simply move onto the sub-expression ahead.
+ *
+ * If we encounter a closing bracket, then we check the element on top of the stack.
+ * If the element at the top of the stack is an opening bracket of the same type, then we pop it off the stack and continue processing.
+ * Else, this implies an invalid expression.
+ *
+ * In the end, if we are left with a stack still having elements, then this implies an invalid expression.
+ */
 
-  const isLeftExpression = expr => {
-    return leftBrackets.includes(expr);
+// This solution is better than that garbage I had here before
+// solution by @bradmcevilly, leetcode
+var isBalanced = function(s) {
+  let closeMap = {
+      ')' : '(',
+      '}' : '{',
+      ']' : '['
   };
+  let charStack = [];
 
-  const isRightExpression = expr => {
-    return rightBrackets.includes(expr);
-  };
+  // validate input
+  if(s === null || s === undefined)
+      return false;
 
-  const doBracketsMatch = (left, right) => {
-    return leftBrackets.indexOf(left) === rightBrackets.indexOf(right);
-  };
+  for(var i = 0; i<s.length; i++){
+      let curChar = s.charAt(i);
+      let topElement;
 
-  // Push into stack and compare with the next char
-  charArr.forEach(char => {
-    if (isLeftExpression(char)) {
-      stack.push(char);
-    }
-    if (isRightExpression(char)) {
-      if (stack.isEmpty()) return false;
-      const top = stack.pop();
-      if (doBracketsMatch(top, char)) {
-        return true;
+      if(closeMap[curChar] !== undefined){
+          topElement = (charStack.length===0) ? '#' : charStack.pop();
+
+          if(topElement !== closeMap[curChar]) {
+              return false;
+          }
+      } else {
+          charStack.push(curChar);
       }
-    }
-  });
+  }
 
-  return stack.isEmpty();
-}
+  return charStack.length === 0;
+};
 
 // console.log(isBalanced('(]'));
