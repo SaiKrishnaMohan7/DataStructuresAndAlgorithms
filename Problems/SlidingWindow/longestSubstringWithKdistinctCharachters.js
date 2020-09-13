5bd5ed126c4a2e52e176390703655467bfdb5af4
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2020-09-12 23:16:39 -0400

    Similar problems

--- Problems/SlidingWindow/longestSubstringWithKdistinctCharachters.js
+++ Problems/SlidingWindow/longestSubstringWithKdistinctCharachters.js
@@ -36,3 +36,14 @@ function longestSubstringWithKdistinctCharacters(str, k) {
 // In each step, we will try to shrink the window from the beginning if the count of distinct characters in the HashMap is larger than ‘K’. We will shrink the window until we have no more than ‘K’ distinct characters in the HashMap. This is needed as we intend to find the longest window.
 // While shrinking, we’ll decrement the frequency of the character going out of the window and remove it from the HashMap if its frequency becomes zero.
 // At the end of each step, we’ll check if the current window length is the longest so far, and if so, remember its length.
+
+/**
+ * Similar problems:
+ * Given an array of characters where each character represents a fruit tree, you are given two baskets and your goal is to put maximum number of fruits in each basket. The only restriction is that each basket can have only one type of fruit.
+
+You can start with any tree, but once you have started you can’t skip a tree. You will pick one fruit from each tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
+
+Write a function to return the maximum number of fruits in both the baskets.
+
+Ans: It is the same as the above function with k = 2
+ */
