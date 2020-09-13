0eeacaf7ac541bb2d40f1c0ca4cb5ab4f534a02e
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2020-09-13 18:09:03 -0400

    Similar problems

--- Problems/SlidingWindow/findLongestSubstringAfterReplacement.js
+++ Problems/SlidingWindow/findLongestSubstringAfterReplacement.js
@@ -31,3 +31,26 @@ const length_of_longest_substring = function(str, k) {
 
   return maxLength;
 };
+
+const findLongestSubArrayOnesAfterReplacement = function(arr, k) {
+  let maxLength = 0;
+  let windowStart = 0;
+  let maxOnesCount = 0;
+
+  for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    if (arr[windowEnd] === 1) {
+      maxOnesCount += 1;
+    }
+
+    if ((windowEnd - windowStart + 1 - maxOnesCount) > k) {
+      if (arr[windowStart] === 1) {
+        maxOnesCount -= 1;
+      }
+      windowStart += 1;
+    }
+
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+
+  return maxLength;
+};
